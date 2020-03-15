4e45c0e3e2def07cbf388ea96ab26852dfdaf1d8
congli <licongmengliu@163.com>
2020-03-15 21:06:04 +0800

    .

---
+++ easyfind-boot/src/main/java/com/stranger/easyfindboot/EasyfindBootApplication.java
@@ -0,0 +1,13 @@
+package com.stranger.easyfindboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyfindBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyfindBootApplication.class, args);
+    }
+
+}
---
+++ easyfind-boot/src/test/java/com/stranger/easyfindboot/EasyfindBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stranger.easyfindboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyfindBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
